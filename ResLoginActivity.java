d05c247b61bb93a3540d40926bd9670bbb367a25
Ishita15112001 <52873398+Ishita15112001@users.noreply.github.com>
2019-12-18 01:18:09 +0530

    Add files via upload
    Firebase Authentication of registered restaurant.

---
+++ ResLoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.foodiesarena;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_res_login);
+    }
+}
---
+++ WelcomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.foodiesarena;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+    }
+}
